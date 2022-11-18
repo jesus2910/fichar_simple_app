71a64dd26bc6204d1363b3aec96d636eadcd8dc7
jesus2910 <jesus29101996@gmail.com>
2022-11-18 16:31:13 +0100

    Imagen Negocio

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
             <div class="mb-md-5 mt-md-4">
 
               <h2 class="fw-bold mb-2 text-uppercase">FICHAJE</h2>
+              <img src="img/1200.png" class="img-fluid" style=" height: 100px;" alt="imagen-negocio">
               <p class="text-white-50 mb-2"> Selecciona tu nombre. </p>
                 <form method="post">
                   <div class="d-grid gap-2 col-12 mx-auto">
